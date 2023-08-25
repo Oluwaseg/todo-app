639c559b903d81b26ac9139dc42fe549ceb7da95
Abhinav Palugula <abhinav.palugula@gmail.com>
2023-08-25 20:25:52 +0530

    version2

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ function addTodo() {
   const compareDates = (d1, d2) => {
     let date1 = new Date(d1).getTime();
     let date2 = new Date(d2).getTime();
-    console.log(time1 < time2);
     if (date1 < date2) {
       alert("The due date is invalid");
       return;
